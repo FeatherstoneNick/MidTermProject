454177aa7acaebc64e44b9ed05fac783f5b7c703
FeatherstoneNick <90656929+FeatherstoneNick@users.noreply.github.com>
2021-10-26 10:24:43 -0400

    Created Repo

---
+++ MidTermGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* 
+ Code Notes!
+
+
+ */
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+
+        }
+    }
+    
+    bool IsGrounded()
+    {
+        //to check if something is in the radius of another object.
+        return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
+        // This allows the player to not be abe to jump in mid air by checking if the player
+        // is standing on the ground.
+    }
+}
